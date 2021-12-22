fca4cb0a4e2f571460b26238704cc27fa50cef7b
CGuillotG <carlos@guillot.dev>
2021-12-22 16:52:26 -0600

    BE - Added Users module

--- BE/src/users/users.controller.ts
+++ BE/src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(@Query() queries) {
+    if (queries && Object.keys(queries).length === 0) {
+      return this.usersService.getAllUsers()
+    } else {
+      return this.usersService.getFilteredUsers(queries)
+    }
+  }
 }
--- BE/src/users/users.service.ts
+++ BE/src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const data = require('../../data.json');
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  getAllUsers() {
+    return data
+  }
+  getFilteredUsers(queries) {
+    return "filtered"
+  }
+}
